68776ac1bdf66c49ada5faffd3d006fe04d27063
Pramath Joshi <45024617+pramjoshcyb@users.noreply.github.com>
2018-11-25 23:40:57 -0500

    updated scrypt.js file

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,16 @@
-function myFunction(){
+function Melbourne(){
     alert("Hello");
+    window.open("Melbourne.html");
+}
+
+
+function Sydney(){
+    alert("Welcome to Sydney");
+    window.open("Sydney.html");
+
+}
+
+function Canberra(){
+    alert("Welcome to Canberra");
+    window.open("Canberra.html");
 }
